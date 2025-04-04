f782d4d6275ba867064b81c792e417800e5822fb
hoangngocnguyen <nguyenhoang19112005@gmail.con>
2025-04-04 21:40:16 +0700

    edit local

--- hi.cpp
+++ hi.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main() {
     cout<<"Hello"<<endl;
-    cout<<"Viết dòng này để tạo conflict";
-
     cout<<"Cứ lỗi đi";
+
+    
     cout<<"Hoang dep trai nhat the gioi"<<endl;
     system("pause");
 }
